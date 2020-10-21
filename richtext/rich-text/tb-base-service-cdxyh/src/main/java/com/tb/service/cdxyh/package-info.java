86e715c4be86ba880743d26d2da05505cbcae043
MaoHaifeng <mhfflying@163.com>
2020-10-21 10:40:17 +0800

    新增校友会工程

---
+++ richtext/rich-text/tb-base-service-cdxyh/src/main/java/com/tb/service/cdxyh/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * @FileName: com.sticker.online.system
+ * @Description:
+ * @Author: Sticker
+ * @Date: 2019/8/29
+ * @Version: 1.0
+ * @LastModified:毛海锋
+ */
+@ModuleGen(name = "tb-base-service-cdxyh", groupPackage = "com.tb.service.cdxyh")
+package com.tb.service.cdxyh;
+
+import io.vertx.codegen.annotations.ModuleGen;
