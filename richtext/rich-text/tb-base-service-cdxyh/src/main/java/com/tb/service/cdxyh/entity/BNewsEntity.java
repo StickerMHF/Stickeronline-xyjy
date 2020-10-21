ef4ebb4f56d744b99b7bd4a4e747ed0e0bd0a048
MaoHaifeng <mhfflying@163.com>
2020-10-21 11:58:20 +0800

    添加demo

---
+++ richtext/rich-text/tb-base-service-cdxyh/src/main/java/com/tb/service/cdxyh/entity/BNewsEntity.java
@@ -0,0 +1,74 @@
+package com.tb.service.cdxyh.entity;
+
+import com.sticker.online.core.entity.BaseEntity;
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.json.JsonObject;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@DataObject(generateConverter = true)
+@Table(name = "b_news")
+public class BNewsEntity extends BaseEntity {
+    public BNewsEntity() {
+    }
+
+    public JsonObject toJson() {
+        JsonObject json = new JsonObject();
+        json = super.toJson();
+        BNewsEntityConverter.toJson(this, json);
+        return json;
+    }
+
+    public BNewsEntity(JsonObject jsonObject) {
+        super(jsonObject);
+        BNewsEntityConverter.fromJson(jsonObject, this);
+    }
+
+
+    /**
+     * id
+     */
+    @Id
+    @GeneratedValue(generator = "jpa-uuid")
+    @GenericGenerator(name = "jpa-uuid", strategy = "uuid")
+    @Column(name = "id",length = 32)
+    private String id;
+
+
+    /**
+     * 新闻标题
+     */
+    @Column(name = "title")
+    private String title;
+    /**
+     * 新闻内容
+     */
+    @Column(name = "contents")
+    private String contents;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
